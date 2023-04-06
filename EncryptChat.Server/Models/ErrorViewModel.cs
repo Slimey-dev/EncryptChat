b0e7242ac54cb1680df0cac196ea02025b14e231
Slimey-dev <75830341+Slimey-dev@users.noreply.github.com>
2023-04-06 10:25:27 +0200

    init
    Took 54 seconds

---
+++ EncryptChat.Server/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace EncryptChat.Server.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
